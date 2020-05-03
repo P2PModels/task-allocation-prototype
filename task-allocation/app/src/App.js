c925fabff844c53a80403ba582e394ae0aba995f
PJColombo <paulojcd2@outlook.com>
2020-05-04 00:23:13 +0200

    App title changed. App main component implemented

--- task-allocation/app/src/App.js
+++ task-allocation/app/src/App.js
@@ -1,87 +1,165 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useAragonApi } from '@aragon/api-react'
-import {
-  Box,
-  Button,
-  GU,
-  Header,
-  IconMinus,
-  IconPlus,
-  Main,
-  SyncIndicator,
-  Tabs,
-  Text,
-  textStyle,
-} from '@aragon/ui'
+import { Main, Header, textStyle, Button, FloatIndicator, LoadingRing, Tabs } from '@aragon/ui'
 import styled from 'styled-components'
 
-function App() {
-  const { api, appState, path, requestPath } = useAragonApi()
-  const { count, isSyncing } = appState
+import Tasks from './screens/Tasks'
+import TasksDnD from './screens/TasksDnD'
 
-  const pathParts = path.match(/^\/tab\/([0-9]+)/)
-  const pageIndex = Array.isArray(pathParts)
-    ? parseInt(pathParts[1], 10) - 1
-    : 0
+import AccountSelector from './screens/AccountSelector'
+
+import AmaraApi from './amara-api/'
+import AdminDashboard from './components/AdminDashboard'
+import { ADMIN_ADDRESS } from './lib/amara-utils'
+
+import { toChecksumAddress } from 'web3-utils'
+
+const tabs = [{name: 'Tasks', body: 'Tasks'}, {name:'Tasks Drag & Drop', body: 'TasksDnD'}]
+
+function App() {
+  const { api, appState, connectedAccount } = useAragonApi()
+  const { amara, apiUrl, isSyncing } = appState
+  const [availableTasks, setAvailableTasks] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [selected, setSelected] = useState(0)
+  const amaraId = amara ? amara.id : undefined
+
+  const ScreenTab = ({ screenName }) => {
+    switch(screenName.toLowerCase()) {
+      case 'tasks':
+        return (
+          <Tasks
+            tasks={availableTasks}
+            isLoading={isLoading}
+            userId={amara.id}
+          />
+        )
+      case 'tasksdnd':
+        return (
+          <TasksDnD
+            tasks={availableTasks}
+            isLoading={isLoading}
+            userId={amara.id}
+          />
+        )
+    }
+  }
+
+  const handleSelectedAccount = useCallback(amara => {
+    try {
+      AmaraApi.users.update(amara.id, { ...amara, active: 1 })
+      api.emitTrigger('AccountSelected', { amara })
+    } catch (err) {
+      console.error(err)
+    }
+  }, [api])
+
+  const handleDisconnectAccount = useCallback(() => {
+    amara.id && AmaraApi.users.update(amara.id, { ...amara, active: 0 }).then(
+      () => 
+        api.emitTrigger('AccountDisconnected', {}),
+      err => {
+        setAvailableTasks([])
+        console.error('Error trying to disconnect account', err)
+      }
+    )
+  }, [amaraId, api])
+
+  const handleRestartClick = () => {
+    api.restart().subscribe(
+      () => console.log('Prototype restarted'),
+      err => console.error(err)
+    )
+  }
+
+  useEffect(() => {
+    setAvailableTasks([])
+    if (amara && amara.id && apiUrl) {
+      const { id, teams } = amara
+      AmaraApi.setApiKeyHeader(amara.apiKey)
+      AmaraApi.setBaseUrl(apiUrl)
+      setIsLoading(true)
+      AmaraApi.users.getUserAvailableTasks(id, teams).then(
+        tasks => {
+          setIsLoading(false)
+          setAvailableTasks(tasks)
+        },
+        err =>{
+          setIsLoading(false)
+          console.error('Error trying to get user available tasks', err)
+        }
+      )
+    }
+  }, [amaraId, apiUrl])
 
   return (
     <Main>
-      {isSyncing && <SyncIndicator />}
-      <Header
-        primary="Counter"
-        secondary={
-          <Text
-            css={`
-              ${textStyle('title2')}
-            `}
-          >
-            {count}
-          </Text>
-        }
-      />
-      <Tabs
-        items={['Tab 1', 'Tab 2']}
-        selected={pageIndex}
-        onChange={index => requestPath(`/tab/${index + 1}`)}
-      />
-      <Box
-        css={`
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          text-align: center;
-          height: ${50 * GU}px;
-          ${textStyle('title3')};
-        `}
-      >
-        Count: {count}
-        <Buttons>
-          <Button
-            display="icon"
-            icon={<IconMinus />}
-            label="Decrement"
-            onClick={() => api.decrement(1).toPromise()}
+      {isSyncing && <Syncing />}
+      {amaraId ? (
+        <React.Fragment>
+          <div css={`margin-top: 4%;`}>
+            <Tabs
+              items={tabs.map(t => t.name)}
+              selected={selected}
+              onChange={setSelected}
+            />
+          </div>
+          <Header
+            primary={
+              <HeaderLayout>
+              <div
+                css={`
+                  ${textStyle('title2')}
+                `}
+              >
+                Task Allocation
+              </div>
+              <Button label="Disconnect" mode="negative" onClick={handleDisconnectAccount} />
+              </HeaderLayout>
+            }
           />
-          <Button
-            display="icon"
-            icon={<IconPlus />}
-            label="Increment"
-            onClick={() => api.increment(1).toPromise()}
-            css={`
-              margin-left: ${2 * GU}px;
-            `}
-          />
-        </Buttons>
-      </Box>
+          <ScreenTab screenName={tabs[selected].body} />
+        </React.Fragment>
+      ) : (
+        <AccountSelectorLayout>
+          <AccountSelector onSelectAccount={handleSelectedAccount} />
+        </AccountSelectorLayout>
+      )}
+      {isLoading && (
+        <FloatIndicator>
+          <LoadingRing />
+          Fetching available tasks
+        </FloatIndicator>
+      )}
+      {connectedAccount && toChecksumAddress(connectedAccount) === toChecksumAddress(ADMIN_ADDRESS) && 
+            <AdminDashboardLayout>
+              <AdminDashboard onClickChangeBaseUrl={baseUrl => api.setApiUrl(baseUrl).toPromise()} onClickRestart={handleRestartClick} />
+            </AdminDashboardLayout>}
     </Main>
   )
 }
 
-const Buttons = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  grid-gap: 40px;
-  margin-top: 20px;
+const AdminDashboardLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10%;
+`
+const Syncing = styled.div.attrs({ children: 'Syncing…' })`
+  position: absolute;
+  top: 15px;
+  right: 20px;
 `
 
+const HeaderLayout = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
+const AccountSelectorLayout = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 export default App
