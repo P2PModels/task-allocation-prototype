96988353817bfa89d6135ba2d328cb7832f1af44
PJColombo <paulojcd2@outlook.com>
2020-05-17 23:28:52 +0200

    Refactoring api handler code to add subtitle requests logic

--- task-allocation/app/src/amara-api/api-helper.js
+++ task-allocation/app/src/amara-api/api-helper.js
@@ -1,9 +1,25 @@
 import { instance } from './client'
 
+function flatQueryParams(queryParams) {
+  if (queryParams)
+    return Object.keys(queryParams).reduce((prevQueries, queryKey, index) => {    
+      return `${prevQueries}${index > 0 ? '&' : ''}${queryKey}=${queryParams[queryKey]}`
+    }, '?')
+  return ''
+}
+
 function createBasicCRUDEndpoints(name) {
   const endpoints = {}
 
-  endpoints.getOne = (id, query) => instance.get(`/${name}/${id}`)
+  endpoints.getAll = query => {
+    const formattedQuery = flatQueryParams(query)
+    return instance.get(`/${name}${formattedQuery}`)
+  }
+
+  endpoints.getOne = (id, query) => {
+    const formattedQuery = flatQueryParams(query)
+    return instance.get(`/${name}/${id}${formattedQuery}`)
+  }
 
   endpoints.update = (id, modifiedEntity) =>
     instance.post(`/${name}/${id}`, modifiedEntity)
@@ -11,4 +27,6 @@ function createBasicCRUDEndpoints(name) {
   return endpoints
 }
 
-export { createBasicCRUDEndpoints }
+
+const AVAILALBLE_SUBTITLE_REQUESTS_QUERY = {'work_status': 'needs-subtitler'}
+export { createBasicCRUDEndpoints, flatQueryParams, AVAILALBLE_SUBTITLE_REQUESTS_QUERY }
