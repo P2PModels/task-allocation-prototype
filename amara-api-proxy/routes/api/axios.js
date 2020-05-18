5a8e47b1fd2e6966a636453d982188b66931b88a
PJColombo <paulojcd2@outlook.com>
2020-05-19 00:36:10 +0200

    staging server baseURL added. Subtitle requests route added. Minor changes made to existing routes

--- amara-api-proxy/routes/api/axios.js
+++ amara-api-proxy/routes/api/axios.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
 const instance = axios.create({
-  baseURL: 'https://amara.org/api/',
+  // baseURL: 'https://amara.org/api/',
+  baseURL: 'https://staging.amara.org/api/'
 })
 
 module.exports = instance
---
+++ amara-api-proxy/routes/api/teams/subtitle-requests/getSubtitleRequests.js
@@ -0,0 +1,25 @@
+const Router = require('express').Router
+const axios = require('../../axios')
+
+
+module.exports = Router({ mergeParams: true })
+    .get('/teams/:slug/subtitle-requests', async (req ,res, next) => {
+        try {
+            let resJson = { meta: { total_count: 0}, objects: [] }
+            const { data: { meta: subReqMeta, objects: subReqObjects }} =
+                await axios.get(`/teams/${req.params.slug}/subtitle-requests/${req.query.limit ? `?limit=${req.query.limit}` : ''}
+                    ${req.query.work_status ? `&work_status=${req.query.work_status}` : ''}`)
+            
+            resJson.meta.total_count += subReqMeta.total_count
+            resJson.objects.push(...subReqObjects)
+            if(req.query.username) {
+                const { data: { meta: userSubReqMeta, objects: userSubReqObjects }} = 
+                    await axios.get(`/teams/${req.params.slug}/subtitle-requests/?assignee=${req.query.username}`)
+                resJson.meta.total_count += userSubReqMeta.total_count
+                resJson.objects.push(...userSubReqObjects)
+            }
+            return res.json(resJson)
+        } catch(error) {
+            next(error)
+        }
+    })
