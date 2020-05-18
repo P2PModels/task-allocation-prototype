61122481fa2f81e0558557dade5b58fe31e75c94
PJColombo <paulojcd2@outlook.com>
2020-05-18 08:44:59 +0200

    Refactoring custom middlewares

--- amara-api-proxy/middlewares/addQueryParameters.middleware.js
+++ amara-api-proxy/middlewares/addQueryParameters.middleware.js
@@ -1,7 +1,10 @@
 module.exports = function queryParamsHandler(req, res, next) {
     /* Build query parameters */
-    req.queryParams = Object.keys(req.query).reduce((queryParams, param, index) => {
-      return queryParams += (index > 0 ?  `&` : ``) + `${param}=${req.query[param]}`
-    }, `?`)
+    if (req.query && Object.keys(req.query).length)
+      req.queryParams = Object.keys(req.query).reduce((queryParams, param, index) => {
+        return queryParams += (index > 0 ?  `&` : ``) + `${param}=${req.query[param]}`
+      }, `?`)
+    else
+      req.queryParams = ``
     next()
 }
