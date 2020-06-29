584ed8afd7302bb38d055fc28d6495a3b6fade3f
PJColombo <paulojcd2@outlook.com>
2020-06-29 12:50:47 +0200

    Error card created

---
+++ task-allocation/app/src/components/Cards/ErrorCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {
+  EmptyStateCard,
+} from '@aragon/ui'
+
+const ErrorCard = ({ msg }) => {  
+  return (
+    <div>
+      <EmptyStateCard
+        text={
+          <span>
+            {msg}
+          </span>
+        }
+      />
+    </div>
+  )
+}
+
+export default ErrorCard
