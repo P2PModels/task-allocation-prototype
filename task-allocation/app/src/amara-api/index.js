57aa3e351f06a59ccc9df0387b75fe3cac2a1855
PJColombo <paulojcd2@outlook.com>
2020-06-29 12:49:54 +0200

    Rating Api file added to Api object

--- task-allocation/app/src/amara-api/index.js
+++ task-allocation/app/src/amara-api/index.js
@@ -2,11 +2,13 @@ import { setApiKeyHeader, setBaseUrl } from './client'
 import Teams from './Teams'
 import Users from './Users'
 import Videos from './Videos'
+import Ratings from './Ratings'
 
 const Api = {
   teams: Teams,
   users: Users,
   videos: Videos,
+  ratings: Ratings,
   setApiKeyHeader,
   setBaseUrl,
 }
