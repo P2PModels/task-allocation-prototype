d9b9591e6f1bd62ed4a066e39e90d16891e4d989
PJColombo <paulojcd2@outlook.com>
2020-08-15 13:06:16 +0200

    getting port from env variables

--- amara-api-proxy/index.js
+++ amara-api-proxy/index.js
@@ -1,19 +1,24 @@
 const ngrok = require('ngrok')
 
 const app = require('./createExpressApp')()
-const port = process.env.PORT || 5000
+const { PROXY_API_PORT } = require('./config')
+// const {
+//   startRoundRobin,
+// } = require('./ethereum/task-allocation-models/round-robin')
+
+const port = PROXY_API_PORT
 
 app.listen(port, err => {
-  if(err)
-    console.log(`Couldn't run server. ${err.message}`);
+  if (err) console.log(`Couldn't run server. ${err.message}`)
   else {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`)
 
     ngrok.connect(port).then(
-      url => console.log(`Server is publicly-accesible at ${url}`),
+      url => {
+        console.log(`Server is publicly-accesible at ${url}`)
+        // startRoundRobin(true)
+      },
       err => console.log('Error trying to initialize ngrok', err)
     )
-
   }
 })
-
