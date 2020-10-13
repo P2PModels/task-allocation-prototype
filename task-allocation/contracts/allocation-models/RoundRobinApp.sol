f2fc9ef1ee92d587817228d3c6d0d4084e625a42
PJColombo <paulojcd2@outlook.com>
2020-10-13 15:44:16 +0200

    App contract: allocation operation refactored, max task allocation number added. Task allocation creation separated from task allocation

--- task-allocation/contracts/allocation-models/RoundRobinApp.sol
+++ task-allocation/contracts/allocation-models/RoundRobinApp.sol
@@ -9,6 +9,7 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
     using SafeMath for uint256;
 
     /// Events
+    event TaskCreated(bytes32 indexed taskId);
     event TaskAccepted(bytes32 indexed userId, bytes32 indexed taskId);
     event TaskRejected(bytes32 indexed userId, bytes32 indexed taskId);
     event TaskAllocated(bytes32 indexed userId, bytes32 indexed taskId, bytes32 previousUserId);
@@ -44,17 +45,20 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
         uint256 benefits;
         bool available;
         bool exists;
+        mapping(uint256 => bytes32) allocatedTasks;
+        uint256 allocatedTasksLength;
     }
 
     struct Task {
-        uint256 reallocationThreshold;
         uint256 userIndex;
+        uint256 allocationIndex;
+        uint256 endDate;
         Status status;
         // bytes32 languageGroup;
         mapping(bytes32 => bool) rejecters;
     }
 
-
+    uint8 constant public MAX_ALLOCATED_TASKS = 3;
 
     ///Errors
     string private constant ERROR_ASSIGNED_TASK = "TASK_ALREADY_ASSIGNED";
@@ -63,7 +67,9 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
     string private constant ERROR_USER_DONT_EXIST = "USER_DONT_EXIST";
     string private constant ERROR_TASK_EXISTS = "TASK_EXISTS";
     string private constant ERROR_TASK_DONT_EXIST = "TASK_DONT_EXIST";
+    string private constant ERROR_TASK_ALLOCATION = "TASK_ALLOCATION_FAIL";
     string private constant ERROR_TASK_NOT_ASSIGNED_TO_USER = "TASK_NOT_ASSIGNED_TO_USER";
+    string private constant ERROR_USER_HAS_TOO_MANY_TASKS = "USER_HAS_TOO_MANY_TASKS";
 
     modifier userHasNoTask(bytes32 _userId) {
         require(!userTaskRegistry[_userId], ERROR_USER_HAS_TASK);
@@ -97,10 +103,15 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
         _;
     }
 
+    modifier tooManyTasks(bytes32 _userId) {
+        require(users[_userId].allocatedTasksLength <=  MAX_ALLOCATED_TASKS, ERROR_USER_HAS_TOO_MANY_TASKS);
+        _;
+    }
+
     function initialize() public onlyInit {
         initialized();
-        reallocationTime = 1800;
-        // reallocationTime = 90;
+        // reallocationTime = 1800;
+        reallocationTime = 90;
     }
 
     function restart() external {
@@ -120,6 +131,36 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
         emit UserRegistered(_userId);
     }
 
+    /**
+     * @notice Create a new assignment.
+     * @param _taskId The task's id.
+     */
+    function createTask(
+        // bytes32 _languageGroup,
+        bytes32 _taskId
+    )
+    external
+    taskDontExist(_taskId)
+    {
+        Task storage task = tasks[_taskId];
+        task.status = Status.Available;
+
+        emit TaskCreated(_taskId);
+    }
+
+    function allocateTask(
+        bytes32 _taskId,
+        bytes32 _userId
+    )
+    external
+    taskExists(_taskId)
+    userExists(_userId)
+    {
+        bool allocated = addUserAllocatedTask(_userId, _taskId);
+        require(allocated, ERROR_TASK_ALLOCATION);
+        emit TaskAllocated(_userId, _taskId, 0);
+    }
+
     function acceptTask(
         bytes32 _userId,
         bytes32 _taskId
@@ -131,6 +172,17 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
     {
         tasks[_taskId].status = Status.Accepted;
         userTaskRegistry[_userId] = true;
+        //Can't access mapping inside user struct
+        //unless It's declare using storage keyword.
+        User storage user = users[_userId];
+        bytes32 currentTaskId;
+        //Reallocated all user available tasks.
+        for (uint i = 0; i < user.allocatedTasksLength; i++) {
+            currentTaskId = user.allocatedTasks[i];
+            if (currentTaskId != _taskId) {
+                reallocateTask(currentTaskId);
+            }
+        }
         emit TaskAccepted(_userId, _taskId);
     }
 
@@ -146,29 +198,6 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
         emit TaskRejected(_userId, _taskId);
         reallocateTask(_taskId);
     }
-    /**
-     * @notice Create and allocate a new assignment. We will receive the initial random assignee
-     * @param _userId The user's id.
-     * @param _taskId The task's id.
-     */
-    function createTask(
-        // bytes32 _languageGroup,
-        bytes32 _userId,
-        bytes32 _taskId
-    )
-    external
-    taskDontExist(_taskId)
-    userHasNoTask(_userId)
-    {
-        uint256 uIndex = users[_userId].index;
-        //Use fancy struct instantiation to omit mapping declaration.
-        tasks[_taskId] = Task({
-            reallocationThreshold: block.timestamp.add(reallocationTime),
-            status: Status.Assigned,
-            userIndex: uIndex
-        });
-        emit TaskAllocated(_userId, _taskId, "");
-    }
 
     function reallocateTask(
         bytes32 _taskId
@@ -178,23 +207,21 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
     {
         Task storage task = tasks[_taskId];
         uint oldUserIndex = task.userIndex;
+        bytes32 oldUserId = userIndex[oldUserIndex];
         uint newUserIndex = oldUserIndex.add(1);
         bool assigneeFounded = false;
         uint userCounter = 0;
 
+        deleteUserAllocatedTask(oldUserId, _taskId);
         while (userCounter < userIndexLength && !assigneeFounded) {
             if (newUserIndex == userIndexLength) {
                 newUserIndex = 0;
             }
             bytes32 currUserId = userIndex[newUserIndex];
             User memory currUser = users[currUserId];
-            // User doesn't have a task already and didn't reject current task.
-            if (currUser.available && !userTaskRegistry[currUserId] && !task.rejecters[currUserId]) {
-                assigneeFounded = true;
-                task.userIndex = newUserIndex;
-                task.status = Status.Assigned;
-                task.reallocationThreshold = block.timestamp.add(reallocationTime);
-                emit TaskAllocated(userIndex[newUserIndex], _taskId, userIndex[oldUserIndex]);
+            assigneeFounded = addUserAllocatedTask(currUserId, _taskId);
+            if (assigneeFounded) {
+                emit TaskAllocated(userIndex[newUserIndex], _taskId, oldUserId);
             } else {
                 userCounter = userCounter.add(1);
                 newUserIndex = newUserIndex.add(1);
@@ -205,53 +232,68 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
         }
     }
 
+    function addUserAllocatedTask(
+        bytes32 _userId,
+        bytes32 _taskId
+    )
+    private
+    returns (bool)
+    {
+        User storage user = users[_userId];
+        Task storage task = tasks[_taskId];
+
+        // User doesn't have a task already and didn't reject current task.
+        if (user.available && !userTaskRegistry[_userId] && !task.rejecters[_userId]
+            && user.allocatedTasksLength <=  MAX_ALLOCATED_TASKS) {
+
+            user.allocatedTasks[user.allocatedTasksLength] = _taskId;
+            task.allocationIndex = user.allocatedTasksLength;
+            user.allocatedTasksLength = user.allocatedTasksLength.add(1);
+
+            task.userIndex = user.index;
+            task.status = Status.Assigned;
+            task.endDate = block.timestamp.add(reallocationTime);
+
+            return true;
+        }
+        return false;
+    }
+
+    function deleteUserAllocatedTask(
+        bytes32 _userId,
+        bytes32 _taskId
+    )
+    private
+    {
+        User storage user = users[_userId];
+        uint256 lastTaskIndex;
+        bytes32 lastTask;
+        uint256 allocatedTaskIndex = tasks[_taskId].allocationIndex;
+        if(user.allocatedTasksLength > 0) {
+            lastTaskIndex = user.allocatedTasksLength.sub(1);
+
+        }
+        else {
+            lastTaskIndex = user.allocatedTasksLength;
+        }
+        lastTask = user.allocatedTasks[lastTaskIndex];
+        user.allocatedTasksLength = lastTaskIndex;
+        user.allocatedTasks[allocatedTaskIndex] = lastTask;
+    }
+
     function getUser(
         bytes32 _userId
     )
     external
     view
-    returns(bool)
-    {
-        return users[_userId].exists;
-    }
-
-    function getUserIndex(
-        bytes32 _userId
-    )
-    external
-    view
-    returns(uint256)
-    {
-        return users[_userId].index;
-    }
-
-    function getUserByIndex(
-        uint256 _index
-    )
-    external
-    view
-    returns(bytes32)
-    {
-        return userIndex[_index];
-    }
-
-    function getUsersCount()
-    external
-    view
-    returns (uint256) {
-        return userIndexLength;
-    }
-
-    function getTaskUserIndex(bytes32 _taskId)
-    external
-    view
-    taskExists(_taskId)
     returns (
-        uint256 userIndex
-    ) 
+        bool available,
+        uint256 benefits
+    )
     {
-        Task memory task = tasks[_taskId];
-        userIndex = task.userIndex;
+        User memory user = users[_userId];
+        available = user.available;
+        benefits = user.benefits;
     }
 
     function getTask(bytes32 _taskId)
@@ -259,10 +301,12 @@ contract RoundRobinApp is AragonApp, BaseTaskAllocation {
     view
     taskExists(_taskId)
     returns (
-        uint256 endDate
+        uint256 endDate,
+        Status status
     ) 
     {
         Task memory task = tasks[_taskId];
-        endDate = task.reallocationThreshold;
+        endDate = task.endDate;
+        status = task.status;
     }
 }
