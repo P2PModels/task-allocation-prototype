4f9c11e929d2f15afb41c29fbc228be8fba6d7ce
PJColombo <paulojcd2@outlook.com>
2020-11-05 05:07:52 +0100

    main scripts added

---
+++ transaction-manager/scripts/create-mock-data.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const { generateMockData } = require('../ethereum/task-allocation-models/round-robin/mock-data-helpers')
+
+generateMockData()
---
+++ transaction-manager/scripts/start-event-listeners.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const { setUpEventListeners } = require('../ethereum/task-allocation-models/round-robin/event-listener-helpers')
+
+setUpEventListeners()
