7cdb0b4b4dd1480d02e6f12488b0857447c8dc27
PJColombo <paulo.colombo@pm.me>
2021-03-25 13:58:51 +0100

    user parser refactored

--- task-allocation/connector/src/thegraph/parsers/users.ts
+++ task-allocation/connector/src/thegraph/parsers/users.ts
@@ -1,11 +1,15 @@
 import { QueryResult } from '@aragon/connect-thegraph'
 import { ErrorUnexpectedResult } from '../../errors'
 import User from '../../models/User'
+import { TaskData, ASSIGNED_STATUS, ACCEPTED_STATUS } from '../../types'
 
 export function parseUser(result: QueryResult, connector: any): User | null {
   const user = result.data.user
 
   if (!user) throw new ErrorUnexpectedResult('Unable to parse user.')
 
-  return user ? new User(user, connector) : null
+  const data = {
+    ...user,
+  }
+  return user ? new User(data, connector) : null
 }
